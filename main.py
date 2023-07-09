54b14be395744d5f9a8618909a3d29a5e8eff908
Maxim <maxim@exm.com>
2023-07-09 22:30:37 +0300

    (modify) Попробовал еще методы FastApi, изменил main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+fake_db = [
+    {"id": 1, "name": "boris"},
+    {"id": 2, "name": "cvas"},
+    {"id": 11, "name": "tars"},
+]
 
-@app.get("/")
-def main():
-    return "Hello, World!"
+fake_trades = [ {"id": 1, "user_id": 11}, {"id": 2, "user_id": 2} ]
+
+app = FastAPI(
+    title="Work with DB"
+)
+
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int ):
+    return [user for user in fake_db if user.get("id") == user_id]
+
+@app.get("/trades")
+def get_trades(limit: int = 1, offset: int = 0):
+    return fake_trades[limit:][:offset]
+
+@app.post("/users/{user_id}")
+def add_new_user(user_id: int, name: str):
+    fake_db.append({"id": user_id, "name": name})
+    return {"status": 200, "data": fake_db}
